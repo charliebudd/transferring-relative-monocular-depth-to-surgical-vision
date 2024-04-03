751f559ffdf702f85ecbbb3f2a4b31e62135d2ec
mhubii <m.huber_1994@hotmail.de>
2024-04-03 12:14:36 +0100

    updated test: MODEL -> WEIGHTS_URL

--- tests/test_model_availability.py
+++ tests/test_model_availability.py
@@ -1,10 +1,10 @@
 import urllib.request
 
-from trmdsv import MODEL
+from trmdsv import WEIGHTS_URL
 
 
 def test_model_availability() -> None:
-    for model in MODEL:
+    for model in WEIGHTS_URL:
         if not model.name.startswith("_"):
             print(f"Checking model: {model.name}, from url: {model.value}")
             response = urllib.request.urlopen(model.value)
