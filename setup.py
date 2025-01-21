58cf693d4e179b75f8978b5fccb5cbb0aea8da55
charliebudd <charles.budd@kcl.ac.uk>
2025-01-21 10:36:55 +0000

    fix install

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     author_email='charles.budd@kcl.ac.uk',
     url='https://github.com/charliebudd/transferring-relative-monocular-depth-to-surgical-vision',
     license='MIT',
-    packages=['trmdsv'],
-    package_dir={'trmdsv': 'src/trmdsv'},
+    packages=find_packages(where='src', include=['trmdsv', 'trmdsv.*']),
+    package_dir={'': 'src'},
     install_requires=[
         'torch>=2.2.0',
         'torchvision>=0.17.0',
